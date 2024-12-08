01a3171661dc19d7340429f45b0092b4d6553597
Ekaterina Zyrianova <finch976@gmail.com>
2024-12-08 19:18:46 +0500

    all_files

---
+++ Python/lesson1.py
@@ -0,0 +1,24 @@
+"""Модуль по первому уроку."""
+
+# 1. Алгоритм принятия домашки
+# ![image.png](attachment:image.png)
+# 2. Нажимаем на fetchorigin next pullorigin
+# 3. Go to history
+# ![image-2.png](attachment:image-2.png)
+#
+# 4. Алгоритм отправки домашки
+# ![image-3.png](attachment:image-3.png)
+# 5. write name of commit
+# ![image-4.png](attachment:image-4.png)
+# pre-commit run --all-files     проверка на линтеры
+# 6. commit to main
+# 7. push
+# ![image-5.png](attachment:image-5.png)
+#
+#
+
+("Chapter_1_Introduction_to_Data_Science_and_Programming_Basics.ipynb").lower()
+
+str
+
+# 1. линтер - это инструмент анализа и поиска ошибок
---
+++ Python/made-easy/test.py
@@ -0,0 +1,7 @@
+"""Test."""
+
+1 + 1
+
+2 + 2
+
+# ![image.png](attachment:image.png)
