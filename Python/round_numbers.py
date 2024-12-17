a971dc685436ae0a4a29b7ba87e4dfe1f36207eb
Ekaterina Zyrianova <finch976@gmail.com>
2024-12-17 12:10:52 +0500

    docs: add new documentation

--- Python/round_numbers.py
+++ Python/round_numbers.py
@@ -3,6 +3,6 @@
 import random
 
 # +
-number_1 = random.random()
+number_1 = random.random()  # генерируем число от 0 до 1
 
-print(round(number_1, 2))
+print(round(number_1, 2))  # выводим на печать
