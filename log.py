5d7edaf4cdcf9b534439fdeb2780f07680cea049
Ксения <trofimova.kv93@gmail.com>
2025-01-27 13:03:26 +0300

    Выполнен пункт 7 интро. Создан файл лога.
    Создан файл log.ipynb. Внесена информация о выполненных действиях.

---
+++ log.py
@@ -0,0 +1,18 @@
+"""Log file for tracking learning progress and completed materials.
+
+This file contains a daily record of completed tasks, installed software, and
+learning achievements in chronological order.
+"""
+
+# 27/01/25
+#
+# 1. Создала аккаунт на GitHub и оформила его, использовав примеры из шаблонов.
+# 2. Создала аккаунт на Kaggle и получила статус контрибьютора.
+# 3. Создала аккаунт на ODS.AI.
+# 4. Скачала и установила GitHub Desktop и Cursor, проверила настройки ранее установленного VS Code.
+# 5. Установила плагины, настроила Cursor и VS Code к работе.
+# 6. Установила CPython, Git и Conda.
+# 7. Создала джамборд для учебы в canva.
+# 8. Склонировала репозиторий, присоединилась к команде и создала свою ветку.
+# 9. Запустила проверку файлов в репозитории командой pre-commit run --all-files.
+# 10. Ознакомилась с процессом принятия и отправки коммитов.
