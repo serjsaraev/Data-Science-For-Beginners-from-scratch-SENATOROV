ca789693741a08a5bf470f602363383e936a3517
Incognito <hack@lenovo.lan>
2025-01-30 16:40:28 +0300

    update system files
    title:
    add log.ipynb file
    
    description:
    created a new Jupyter notebook for logging purpose

---
+++ log.py
@@ -0,0 +1,6 @@
+"""Лог выполнения."""
+
+# 29/01
+#
+# 1. Запустил следующие команды: git init, pip install pre-commit, pre-commit install, pre-commit run --all-files
+# 2. Создал файл log.ipynb для отслеживания шагов
