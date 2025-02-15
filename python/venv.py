ff1bbcdf0462cb15fee907e2768277dbbf9c542b
Ксения <trofimova.kv93@gmail.com>
2025-02-15 21:53:48 +0300

    commit for check pictures

--- python/venv.py
+++ python/venv.py
@@ -1,32 +1,31 @@
 """Ответы на вопросы по виртуальному окружению."""
 
-# ## Список вопроов и ответов по теме виртуальное окружение:
+# ## Список вопросов и ответов по теме виртуальное окружение:
 #
 # 1. Что делает команда python -m venv venv?
 #
 # Команда для создания виртуального окружения внутри проекта.
 #
 # 1.1 Что делает каждая команда в списке ниже?
-# ![png_1](https://private-user-images.githubusercontent.com/55090151/398252421-62884319-9020-4c01-a4b2-2b956a34ecab.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDMzMzgsIm5iZiI6MTczOTU0MzAzOCwicGF0aCI6Ii81NTA5MDE1MS8zOTgyNTI0MjEtNjI4ODQzMTktOTAyMC00YzAxLWE0YjItMmI5NTZhMzRlY2FiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAyMTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMjE0VDE0MjM1OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWYyMDIzNzE5YmE5MzNmMTk0MDcwY2M2MjY2YzY0YTc0MzFlODYxODE1MzdmNGNmYmFmODU5M2RlYjllYWVmYzQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.Bc8o5blrQpUMCNtFA1xyP3Ks0OFcychTEJBlYRDvBQ4)
 #
-#  - pip list выводит список всех установленных пакетов в вашей текущей среде Python вместе с их версиями.
-#  - pip freeze > requirements.txt используется для создания файла requirements.txt, содержащего список всех установленных пакетов в текущей среде Python и их версий.
-#  - pip install -r requirements.txt используется для установки всех пакетов, которые прописаны в файле requirements.txt
+#  - ```pip list``` - выводит список всех установленных пакетов в вашей текущей среде Python вместе с их версиями.
+#  - ```pip freeze > requirements.txt``` - используется для создания файла requirements.txt, содержащего список всех установленных пакетов в текущей среде Python и их версий.
+#  - ```pip install -r requirements.txt``` - используется для установки всех пакетов, которые прописаны в файле requirements.txt
 #
 # 2. Что делает каждая команда в списке ниже?
-# ![png_2](https://private-user-images.githubusercontent.com/55090151/398252102-401cc261-1400-46a9-8b24-ae650d8ce859.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDM5ODgsIm5iZiI6MTczOTU0MzY4OCwicGF0aCI6Ii81NTA5MDE1MS8zOTgyNTIxMDItNDAxY2MyNjEtMTQwMC00NmE5LThiMjQtYWU2NTBkOGNlODU5LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAyMTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMjE0VDE0MzQ0OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTdjZWQ5ODM0ZDUxMDU0NWFhMGEyNTE2ZDc1OGZkYmI3NDNjMWE1ZjI2N2UxZDIwNDIwMWJkZjJlMThkZDcwZmImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.f4MTq-f5kgOaMqXIarDvuqlqkizV548F_kTxN47DuME)
 #
-#  - conda env list используется для отображения списка всех сред (environments), созданных с помощью conda.
-#  - conda create -n env_name python=3.5 используется для создания новой среды с указанной версией Python (в данном случае Python 3.5) с помощью менеджера пакетов conda.
-#  - conda env update -n env_name -f file.yml используется для обновления существующей среды (environment) с именем env_name с помощью файла спецификации file.yml. Файлы спецификации с расширением yml содержат информацию о зависимостях и конфигурации, которые используются для настройки среды или других процессов.
-#  - source activate env_name используется для активации указанной среды в командной оболочке. Однако начиная с версии conda 4.4, рекомендуется использовать команду conda activate env_name.
-#  - source deactivate - для деактивации виртуального окружения. С версии conda 4.4, рекомендуется использовать команду conda deactivate.
-#  - conda clean -a используется для очистки различных кешей и временных файлов, которые создаются и используются conda. Выполнение этой команды помогает освободить место на диске и поддерживать вашу систему в чистоте.
+#  - ```conda env list``` - используется для отображения списка всех сред (environments), созданных с помощью conda.
+#  - ```conda create -n env_name python=3.5``` - используется для создания новой среды с указанной версией Python (в данном случае Python 3.5) с помощью менеджера пакетов conda.
+#  - ```conda env update -n env_name -f file.yml``` - используется для обновления существующей среды (environment) с именем env_name с помощью файла спецификации file.yml. Файлы спецификации с расширением yml содержат информацию о зависимостях и конфигурации, которые используются для настройки среды или других процессов.
+#  - ```source activate env_name``` - используется для активации указанной среды в командной оболочке. Однако начиная с версии conda 4.4, рекомендуется использовать команду conda activate env_name.
+#  - ```source deactivate``` - для деактивации виртуального окружения. С версии conda 4.4, рекомендуется использовать команду conda deactivate.
+#  - ```conda clean -a``` - используется для очистки различных кешей и временных файлов, которые создаются и используются conda. Выполнение этой команды помогает освободить место на диске и поддерживать вашу систему в чистоте.
 #
 # 3. Вставьте скрин вашего терминала, где вы активировали сначала venv, потом conda, назовите окружение "SENATOROV"
-# ![venv](https://private-user-images.githubusercontent.com/196382432/413315428-5d617b01-4bfd-4652-b322-add11db90ca7.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzE1NDI4LTVkNjE3YjAxLTRiZmQtNDY1Mi1iMzIyLWFkZDExZGI5MGNhNy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNWE5NGIxMDk1ZDI3OWFiZjBiZTJmYzM4ZmExZWZlYmNmYWU4MTUyMjlkNzUyMzhlMDc5NjU1NTQ2OTNkOTFkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.y7DRHrws9dekwjds4LbS5HQ-vW0OHsrutU1BT8UPgEA)
 #
-# ![conda](https://private-user-images.githubusercontent.com/196382432/413326598-5ac29a23-5a8a-4204-86fd-a708ffcf2bc8.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzI2NTk4LTVhYzI5YTIzLTVhOGEtNDIwNC04NmZkLWE3MDhmZmNmMmJjOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xNGEzYWRjOGYzMTJiMDFiYWNjYmRhNzlmNTYxMTljMWY3MzYxYjZhYTMzYTYxNjk5YjllYTg0MzJlNzk4MGJlJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.O_0uPG0bud-siSDSC0u1YQFHulw-8T25h08ub8HWA_0)
+# ![Снимок экрана 2025-02-14 151851.png](<attachment:Снимок экрана 2025-02-14 151851.png>)
+#
+# ![Снимок экрана 2025-02-14 161727.png](<attachment:Снимок экрана 2025-02-14 161727.png>)
 #
 # 4. Как установить необходимые пакеты внутрь виртуального окружения для conda/venv?
 #
@@ -38,11 +37,11 @@
 #
 # 5.1 Вставьте скрин, где будет видна папка VENV в вашем репозитории а также файлы зависимостей requirements.txt и environment.yml, файлы должны содержать зависимости
 #
-# ![SENATOROV](https://private-user-images.githubusercontent.com/196382432/413339570-c5a5d7cb-5de4-4fdf-bd85-bf022609013d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzM5NTcwLWM1YTVkN2NiLTVkZTQtNGZkZi1iZDg1LWJmMDIyNjA5MDEzZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wZTcyMDUzYWI1ZDc1NDI5YzVkYTk2NDJiNjE4YzFlOWYwNTdhMTk1NmU1OThlZWI5NTMzZWUyOGQwZTBhZWVmJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.IwPmz2_ODKqNFNpXQXBb7-cd6jCn35mevEB6U3sOSbQ)
+# ![Снимок экрана 2025-02-14 165655.png](<attachment:Снимок экрана 2025-02-14 165655.png>)
 #
-# ![environment](https://private-user-images.githubusercontent.com/196382432/413339870-6c18b3e0-c8b6-46e7-99f3-7c79e6c538a3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzM5ODcwLTZjMThiM2UwLWM4YjYtNDZlNy05OWYzLTdjNzllNmM1MzhhMy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT01ZGRmNzNkZWM0YjFjNWM0ODQxNGM5NTNlZDJjYjM0ZGYzODJkM2Q0NzdkMGI3NDY1NjlmOTc0MGNkNjJlYmM0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.hO1rhwZT59XI1KKtYPkK5plfI7qIpzJDr_mz0RHntD8)
+# ![Снимок экрана 2025-02-14 165729.png](<attachment:Снимок экрана 2025-02-14 165729.png>)
 #
-# ![requirements](https://private-user-images.githubusercontent.com/196382432/413339959-a3feaf73-143a-435d-86bf-b18912d8970d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzM5OTU5LWEzZmVhZjczLTE0M2EtNDM1ZC04NmJmLWIxODkxMmQ4OTcwZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yODkxYjQxYWQ3MTNkNGNlZmNkMzg4ZjQ4ZjAzMDNkODZhZTVjMGNmZDA3ZmY5ZDQ5NWI4Yjg3ZGM5ZTI0M2FhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.3bsykHQ5-pTUexa7X5fQiheYf41lnL3Gzt7J6umM9TE)
+# ![Снимок экрана 2025-02-14 165743.png](<attachment:Снимок экрана 2025-02-14 165743.png>)
 #
 # 6. Что делают эти команды?
 # ```pip install -r requirements.txt``` - устанавливает зависимости, прописанные в файле requirements.txt.
@@ -59,11 +58,11 @@
 #
 # 9. Вставьте скрин где будет видно, Выбор интерпретатора Python (conda) в VS Code/cursor
 #
-# ![Image](https://private-user-images.githubusercontent.com/196382432/413343397-b601cc95-7940-4cef-9a40-12abb8d5d3e4.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzQzMzk3LWI2MDFjYzk1LTc5NDAtNGNlZi05YTQwLTEyYWJiOGQ1ZDNlNC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iODQ3YTY5MDE3MDBlOGM0YTljNGM4MjA2Y2E4MmRkYWRhZTc4YmRkMzU4NjM0MDdkOGQxZmE3MjNiMjRjOTg2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.tB5rff1Mk9VPDr_sBlMeUMFEUBvWYc6bea_zQ0BfWaE)
+# ![Снимок экрана 2025-02-14 170917.png](<attachment:Снимок экрана 2025-02-14 170917.png>)
 #
 # 10. Добавьте в .gitignore папку SENATOROV
 #
-# ![Image](https://private-user-images.githubusercontent.com/196382432/413344711-2a928bd1-af94-4498-b9f5-0f4aaa5b3f5f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk1NDQxNDEsIm5iZiI6MTczOTU0Mzg0MSwicGF0aCI6Ii8xOTYzODI0MzIvNDEzMzQ0NzExLTJhOTI4YmQxLWFmOTQtNDQ5OC1iOWY1LTBmNGFhYTViM2Y1Zi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjE0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxNFQxNDM3MjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yZGE4M2FhZmUwOTFkZWYxNTFiNGZkODk2NDE2ZDBhMzg4ZmFhMmU1YjI0YzZhODBjY2UwOGE1YmE3Mzk3Zjg1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.ngUtdr4lVwEd9DnV-mnWMCwDek85pJaGjT2-Lc4XWJQ)
+# ![Снимок экрана 2025-02-14 171249.png](<attachment:Снимок экрана 2025-02-14 171249.png>)
 #
 # 11. Зачем нужно виртуально окружение?
 #
